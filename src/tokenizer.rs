2435a4d6c07c6d2c6d2293407291c569f11bc575
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-06-19 17:39:13 +0200

    Updates.

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,14 +1,13 @@
 /// Returns a collection of tokens built from the given input.
-pub fn tokenize(input: impl AsRef<str>) -> Vec<String> {
+pub fn tokenize(input: &str) -> Vec<&str> {
   const DOUBLE_QUOTE: char = '"';
   enum State {
     OutsideToken,
     InsideToken(usize),
     InsideQuote(usize),
   }
-  let mut tokens = vec![];
+  let mut tokens: Vec<&str> = vec![];
   let mut state = State::OutsideToken;
-  let input = input.as_ref();
   for (idx, ch) in input.char_indices() {
     match state {
       State::OutsideToken => match ch {
@@ -18,25 +17,25 @@ pub fn tokenize(input: impl AsRef<str>) -> Vec<String> {
       },
       State::InsideToken(start) => match ch {
         DOUBLE_QUOTE => {
-          tokens.push(input[start..idx].to_string());
+          tokens.push(&input[start..idx]);
           state = State::InsideQuote(idx + 1);
         }
         other if other.is_whitespace() => {
-          tokens.push(input[start..idx].to_string());
+          tokens.push(&input[start..idx]);
           state = State::OutsideToken;
         }
         _ => {}
       },
       State::InsideQuote(start) => {
         if ch == DOUBLE_QUOTE {
-          tokens.push(input[start..idx].to_string());
+          tokens.push(&input[start..idx]);
           state = State::OutsideToken;
         }
       }
     }
   }
   match state {
-    State::InsideToken(start) => tokens.push(input[start..].to_string()),
+    State::InsideToken(start) => tokens.push(&input[start..]),
     State::InsideQuote(start) => tokens.append(&mut tokenize(&input[start..])),
     State::OutsideToken => {}
   }
